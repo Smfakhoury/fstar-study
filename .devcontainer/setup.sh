4d00dd2158aa47809ae3899380b2c176e0755aa7
t-rijuljainMS <t-rijuljain@microsoft.com>
2024-08-20 18:41:42 +0000

    get tutorial pdf

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -3,3 +3,5 @@
 wget https://github.com/FStarLang/FStar/releases/download/v2024.01.13/fstar_2024.01.13_Linux_x86_64.tar.gz
 tar -xvzf fstar_2024.01.13_Linux_x86_64.tar.gz
 rm fstar_2024.01.13_Linux_x86_64.tar.gz
+
+wget https://fstar-lang.org/tutorial/proof-oriented-programming-in-fstar.pdf
